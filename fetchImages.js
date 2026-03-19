0fc2d204a2c8a2856c38b924c12126eb37d2eeb7
psr-codes <prakashsinghrawatuk5ile@gmail.com>
2026-03-19 15:30:20 +0530

    added casino images

---
+++ fetchImages.js
@@ -0,0 +1,14 @@
+const url = "https://api.github.com/repos/psr-codes/images/contents/onichan-fun";
+
+async function getImages() {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const images = data
+    .filter(file => file.type === "file")
+    .map(file => file.download_url);
+
+  console.log(images);
+}
+
+getImages();
